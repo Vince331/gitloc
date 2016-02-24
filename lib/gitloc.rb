7355533a42b40b63ea11bec962dd3686193e1f38
Vince Lundgren <vincent.lundgren@gmail.com>
2016-02-24 14:55:45 -0600

    Extract binary code out of lib code

--- lib/gitloc.rb
+++ lib/gitloc.rb
@@ -4,20 +4,19 @@ require 'open3'
 require 'gitloc/version'
 
 class Gitloc
-  def self.call(argv, outstream, errstream)
-    repo = argv.first
+  def self.call(repo)
     Dir.mktmpdir { |dir|
       Dir.chdir dir
       out, err, status = Open3.capture3 'git', 'clone', repo, 'cloned'
       unless status.success?
         errstream.puts out, err
-        exit  "Failed somehow >.<"
+        exit "Failed somehow >.<"
       end
       Dir.chdir 'cloned'
       files = Dir['**/*'].reject { |name| File.directory? name }
-      files.each do |filename|
+      files.map do |filename|
         loc = File.readlines(filename).count { |line| line !~ /^\s*$/ }
-        outstream.puts "#{loc}\t#{filename}"
+        [filename, loc]
       end
     }
   end
